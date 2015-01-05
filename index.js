da9a4e90288a8aded58de820c7e51760f320b751
rantd <rantd.cd@gmail.com>
2015-01-05 15:28:32 +0900

    update index.js and index.html

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 var width = 800;
 var height = 600;
 
-var svg = d3.select("body").append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g")
-    .attr("width", width)
-    .attr("height", height);
+var request = new XMLHttpRequest();
+request.open("GET", "rests_hongo.json", false);
+request.send(null);
+var rests = JSON.parse(request.responseText);
+console.log(rests);
 
-queue()
-  .defer(d3.json, "rests_hongo.json")
-  .defer(d3.json, "lib/tokyo.topojson")
-  .await(ready);
-
-var projection = d3.geo.mercator();
-var path = d3.geo.path()
-  .projection(projection);
-
-function ready(error, rests, topo){
-  if(error){
-    console.log(error);
-  }else{
-    console.log("Data loaded");
-    projection
-      .scale(40000)
-      .center([139.463191, 35.710325]);
-
-
-    var geometries = topojson.feature(topo, topo.objects.tokyo).features;
-    var counties = svg.append("g").selectAll(".ward").data(geometries);
-    counties.enter().insert("path")
-      .attr("class", "ward")
-      .attr("d", path)
-      .style("fill", "#FFF")
-      .style("stroke", "#000");
-
-    var rests_pts = svg.append("g").selectAll("circle").data(rests);
-    rests_pts.enter().append("circle")
-      .attr("r", "3px")
-      .attr("cx", function(d){return projection([d.location.longitude, d.location.latitude])[0];})
-      .attr("cy", function(d){return projection([d.location.longitude, d.location.latitude])[1];})
-      .attr("fill", "red");
+function initialize() {
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(35.712850, 139.757827),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var infowindow = new google.maps.InfoWindow();
+  var marker;
+  for (var i = 0; i < rests.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(rests[i].location.latitude, rests[i].location.longitude),
+      map: map
+    });
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent("<b>" + rests[i].name.name + "<b> <br>" +
+          rests[i].contacts.address + "<br> Tel:" +
+          rests[i].contacts.tel);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  };
 }
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
